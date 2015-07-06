cc51502badf7cffa1a646b4d7c03ecf60415e23e
Alexander Sinyagin <sinyagin.alexander@gmail.com>
2015-07-06 11:45:11 +0500

    Add ethereumjs-lib; Add new plugins in the installation script

--- scripts/install-ether-camp.sh
+++ scripts/install-ether-camp.sh
@@ -5,4 +5,6 @@ git clone https://github.com/etherj/ethergit.sandbox.git
 git clone https://github.com/etherj/ethergit.ethereum.sandbox.git
 git clone https://github.com/etherj/ethergit.guests.count.git
 git clone https://github.com/etherj/ethergit.preview.browser.sandbox.git
+git clone https://github.com/etherj/ether.camp.server.git
+git clone https://github.com/etherj/ethergit.libs.git
 cd ../
