90aab08927887c7a6a4c5e245f4f6aa3956dd07c
Alexander Sinyagin <sinyagin.alexander@gmail.com>
2015-09-24 08:48:57 +0000

    Add Sent Txs Editor plugin

--- configs/client-ether-camp-server.js
+++ configs/client-ether-camp-server.js
@@ -726,6 +726,7 @@ module.exports = function(options) {
         "plugins/ethergit.ethereum.sandbox/account_dialog",
         "plugins/ethergit.ethereum.sandbox/abi_dialog/dialog",
         "plugins/ethergit.ethereum.sandbox/send_to_net_dialog/dialog",
+        "plugins/ethergit.ethereum.sandbox/sent_txs_editor/editor",
         "plugins/ethergit.guests.count/guests_count",
         "plugins/ethergit.preview.browser.sandbox/browser"
     ];
