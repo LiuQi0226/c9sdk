7c9f58756a1ec3e1f3be1d3e972e69ddb0e13a98
Alexander Sinyagin <sinyagin.alexander@gmail.com>
2015-10-02 16:06:47 +0000

    Remove plugins from installing scripts

--- scripts/install-ether-camp-shh.sh
+++ scripts/install-ether-camp-shh.sh
@@ -1,10 +1,8 @@
 cd ./plugins
 git clone ssh://git@github.com/etherj/ethergit.solidity.language.git
 git clone ssh://git@github.com/etherj/ethergit.solidity.compiler.git
-git clone ssh://git@github.com/etherj/ethergit.sandbox.git
 git clone ssh://git@github.com/etherj/ethergit.ethereum.sandbox.git
 git clone ssh://git@github.com/etherj/ethergit.guests.count.git
-git clone ssh://git@github.com/etherj/ethergit.preview.browser.sandbox.git
 git clone ssh://git@github.com/etherj/ether.camp.server.git
 git clone ssh://git@github.com/etherj/ethergit.libs.git
 cd ../
--- scripts/install-ether-camp.sh
+++ scripts/install-ether-camp.sh
@@ -1,10 +1,8 @@
 cd ./plugins
 git clone https://github.com/etherj/ethergit.solidity.language.git
 git clone https://github.com/etherj/ethergit.solidity.compiler.git
-git clone https://github.com/etherj/ethergit.sandbox.git
 git clone https://github.com/etherj/ethergit.ethereum.sandbox.git
 git clone https://github.com/etherj/ethergit.guests.count.git
-git clone https://github.com/etherj/ethergit.preview.browser.sandbox.git
 git clone https://github.com/etherj/ether.camp.server.git
 git clone https://github.com/etherj/ethergit.libs.git
 cd ../
