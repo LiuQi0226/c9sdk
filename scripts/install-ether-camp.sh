224c36209fb477d5152f377376c0e357c9cf7b59
Alex Sinyagin <sinyagin.alexander@gmail.com>
2017-05-11 10:33:32 +0000

    Update the installation script

--- scripts/install-ether-camp.sh
+++ scripts/install-ether-camp.sh
@@ -1,8 +1,55 @@
+#!/bin/bash
+
+while [[ $# -gt 1 ]]
+do
+key="$1"
+
+case $key in
+    --ethergit-ethereum-sandbox-branch)
+    ETHERGIT_ETHEREUM_SANDBOX_BRANCH="$2"
+    shift # past argument
+    ;;
+    --ethergit-solidity-compiler-branch)
+    ETHERGIT_SOLIDITY_COMPILER_BRANCH="$2"
+    shift # past argument
+    ;;
+    --ethergit-libs-branch)
+    ETHERGIT_LIBS_BRANCH="$2"
+    shift # past argument
+    ;;
+    *)
+    # unknown option
+    ;;
+esac
+shift # past argument or value
+done
+
+echo $ETHERGIT_SOLIDITY_COMPILER_BRANCH
+echo $ETHERGIT_ETHEREUM_SANDBOX_BRANCH
+
 cd ./plugins
 git clone https://github.com/etherj/ethergit.solidity.language.git
+
 git clone https://github.com/etherj/ethergit.solidity.compiler.git
+cd ethergit.solidity.compiler
+git checkout $ETHERGIT_SOLIDITY_COMPILER_BRANCH
+cd ..
+
 git clone https://github.com/etherj/ethergit.ethereum.sandbox.git
+cd ethergit.ethereum.sandbox
+git checkout $ETHERGIT_ETHEREUM_SANDBOX_BRANCH
+cd ..
+
 git clone https://github.com/etherj/ethergit.guests.count.git
 git clone https://github.com/etherj/ether.camp.server.git
+
 git clone https://github.com/etherj/ethergit.libs.git
+cd ethergit.libs
+git checkout $ETHERGIT_LIBS_BRANCH
+cd ..
+
+# plugins
+# Oraclize plugin is not publicly available
+#git clone ssh://git@github.com/oraclize/ethereum-studio-ide-plugin.git ethereum.studio.oraclize
+
 cd ../
